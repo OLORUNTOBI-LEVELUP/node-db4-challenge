aa778060b3dc28c4f5861ab341aae798dc35684a
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-18 17:20:40 +0100

    Made changes

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -6,12 +6,12 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("ingredients").insert([
         {
-          ingredient_name: "Ingredient Name 1",
-          ingredient_unit: "Ingredient Unit 1"
+          ingredient_name: "salt",
+          ingredient_unit: "10 units"
         },
         {
-          ingredient_name: "Ingredient Name 2",
-          ingredient_unit: "Ingredient Unit 2"
+          ingredient_name: "pepper",
+          ingredient_unit: "20 units"
         }
       ]);
     });
